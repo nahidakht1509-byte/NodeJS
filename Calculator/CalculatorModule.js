561e7926224a2c82a3b673d5ba4a34576abf69ed
nahidakht1509-byte <nahid.akht1509@gmail.com>
2026-03-31 09:20:52 +0530

    day i1 task
    Day  1 task calculator

---
+++ Calculator/CalculatorModule.js
@@ -0,0 +1,16 @@
+export function Addition(a,b){
+    return a+b;
+}
+
+export function Subtraction(a,b){
+    return a-b;
+}
+
+export function Multipication(a,b){
+    return a*b;
+}
+
+export function Division(a,b){
+    return a/b;
+}
+
